25cb768782ecb6c69008384a9d5e51cc4025650a
samin zavarkesh <samin.zavarkesh@gmail.com>
2020-03-27 19:55:56 +0100

    add TCPDF library

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title style="direction: rtl">digital infusion</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <link type="text/css" rel="stylesheet" href="{{URL::asset('public/css/font-awesome.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{URL::asset('public/css/bootstrap.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{URL::asset('public/css/jquery.bxslider.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{URL::asset('public/css/jquery-ui-1.10.4.custom.min.css')}}">
+    <link type="text/css" rel="stylesheet" href="{{URL::asset('public/css/jquery-ui/jquery-ui.css')}}">
+    <link type="text/css" rel="stylesheet" media="screen" href="{{URL::asset('public/css/jquery.fancybox.min.css')}}">
+
+    <script src="{{URL::asset('public/js/jquery.js')}}"></script>
+    <script src="{{URL::asset('public/js/jquery-migrate-1.2.1.min.js')}}"></script>
+    <script src="{{URL::asset('public/js/bootstrap.js')}}"></script>
+    <script src="{{URL::asset('public/js/global.js')}}"></script>
+    <script src="{{URL::asset('public/js/upd.js')}}"></script>
+    <script src="{{URL::asset('public/js/jquery.fancybox.min.js')}}"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+</head>
+
+<body>
+    <div class="container" style="margin-top: 50px">
+        <div class="alert alert-primary" role="alert">
+            start of the project!
+        </div>
+    </div>
+</body>
+</html>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', 'HomeController@index')->name('index');
+//
+Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
